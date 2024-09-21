277fc13f8b3169b34d5eafe2621d61046850aaee
indrawan14 <muhammadindrawannn14@gmail.com>
2024-09-21 11:32:17 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    public static void main (String[] args) {
+        System.out.println("Nama:M INDRAWAN");
+        System.out.println("NIM:607062430008");
+        System.out.println("Kelas:D3IF-48-01");     
+    }
+}
